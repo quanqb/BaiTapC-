8cdd47d3ac80ad562eefab42a1e176853d9d11f4
Quan16i1 <46445982+quanqb@users.noreply.github.com>
2019-03-28 09:10:51 +0700

    quan ly sinh vien

--- Program.cs
+++ Program.cs
@@ -1,24 +1,66 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 
-namespace Baitap
+namespace vidu
 {
     class Program
     {
         static void Main(string[] args)
         {
-            bai1 n = new bai1();
-            n.Nhap();
-            n.tonga();
-            n.tongb();
-            n.intong();
+            int n;
+            Console.Write("Nhap so luong sinh vien: ");
+            n = Convert.ToInt16(Console.ReadLine());
 
-            bai2 m = new bai2();
-            m.nhap();
-            m.tinhtien();
-            m.intien();
+            sinhvien[] arrsv = new sinhvien[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine("Nhap thong tin sinh vien thu: " + (i + 1).ToString());
+                arrsv[i] = new sinhvien();
+                arrsv[i].Nhap();
+
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arrsv[j].TinhTB() < arrsv[i].TinhTB())
+                    {
+                        sinhvien sx;
+                        sx = arrsv[i];
+                        arrsv[i] = arrsv[j];
+                        arrsv[j] = sx;
+                    }
+                }
+            }
+            float min = arrsv[0].TinhTB();
+            for(int i = 1; i<n; i++)
+            {
+                if(arrsv[i].TinhTB() < min)
+                {
+                    min = arrsv[i].TinhTB();
+                }
+            }
+            Console.Write("Sinh vien co diem trung binh thap nhat la: " +min);
+
+            for(int i = 0; i<n; i++)
+            {
+                if (arrsv[i].masv.Contains("IT"))
+                    Console.WriteLine("Sinh vien ma {0} co diem tb la {1}", arrsv[i].masv.Contains("IT");
+            }
+
+            Console.WriteLine("\nDanh sach sinh vien:");
+            Console.WriteLine("{0,-15}{1,-15}", "masv", "diemtb");
+
+            for (int i = 0; i < n; i++)
+            {
+                arrsv[i].Hienthi();
+            }
+            Console.ReadLine();
+
+            
+
+
+
 
         }
     }
---
+++ sinhvien.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace vidu
+{
+    class sinhvien
+    {
+        public String masv;
+        float toan;
+        float ly;
+        float hoa;
+
+        public sinhvien()
+        {
+            this.masv = "01";
+            this.toan = (float)7.5;
+            this.ly = (float)8.5;
+            this.hoa = (float)6.0;
+        }
+        public sinhvien(String masv, float toan, float ly, float hoa)
+        {
+            this.masv = masv;
+            this.toan = toan;
+            this.ly = ly;
+            this.hoa = hoa;
+        }
+        public void Nhap()
+        {
+            Console.WriteLine("Nhap ma sinh vien:");
+            masv = Console.ReadLine();
+            Console.WriteLine("Nhap diem toan:");
+            toan = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap diem ly:");
+            ly = float.Parse(Console.ReadLine());
+            Console.WriteLine("Nhap diem hoa:");
+            hoa = float.Parse(Console.ReadLine());
+
+        }
+        public float TinhTB()
+        {
+            float diemtb = (this.toan + this.ly + this.hoa) / 3;
+            return diemtb;
+        }
+        
+        public void Hienthi()
+        {
+            Console.WriteLine("{0,-15}{1,-15}", masv,TinhTB());
+        }
+    }
+}
